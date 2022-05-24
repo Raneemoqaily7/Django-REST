946b9e7ac89d38db86641603f0487a72b3482ced
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-05-24 23:02:11 +0300

    custom user

--- book/models.py
+++ book/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=255 ,blank=True, null=True)
+
+
+    def __str__(self):
+        return self.username
+        #return self.email
+        
+
+        
--- book_project/settings.py
+++ book_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book'
 ]
 
 MIDDLEWARE = [
@@ -98,6 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'book.CustomUser'
 
 
 # Internationalization
