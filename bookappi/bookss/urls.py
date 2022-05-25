71f66f7daa3a729145485d4c4183050177e20440
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-05-25 03:01:02 +0300

    api

---
+++ bookappi/bookss/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from bookss.api.viewset import BooksListAPIView ,BookDetailAPIView
+
+urlpatterns=[
+    path('api/v1/books-list' ,BooksListAPIView.as_view(), name = 'books_list'),
+    path('api/v1/<int:pk>' ,BookDetailAPIView.as_view(), name = 'book_detail')
+]
